26dd01245e5875003225a15a05840d1f23f306ce
karyn2 <karynaceron@gmail.com>
2023-11-30 15:39:39 -0500

    creando proyecto

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<br>
+<div class="card w-50 mt-5 mx-auto">
+    <img src="{{ asset('images/udenar.png') }}" class="card-img-top" style="width: 50%;" alt="Logo Udenar">
+    <div class="card-body text-center">
+        <h1 class="card-subtitle mb-4">Electiva de Programación Avanzada 3</h1>
+        <h3 class="mb-3">Integrantes:</h3>
+        <p class="card-text">Angela Karina Ceron Benavidez</p>
+        <p class="card-text">Victor Alfonso Delgado Bolaños</p>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+@stop
+
+@section('content')
+
+    <main class="py-4 p-4">
+        @yield('content')
+    </main>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+@stop
